e9adfe6b6e61d67913c89910d0db7a5abcab1e8b
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-02-24 17:51:36 -0300

    Update Multiplos.jsx

--- exercicios/src/componentes/Multiplos.jsx
+++ exercicios/src/componentes/Multiplos.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//Mostrando que é possível haver mais de um componente dentro de um arquivo
 export const BoaTarde = props =>  <h1>Boa tarde {props.nome}!</h1>
 
 export const BoaNoite = props =>  <h1>Boa Noite {props.nome}!</h1>
