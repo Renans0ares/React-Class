f23f768b4d27f5a29e6d70c8812ca4e6200d561b
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-02-24 18:34:54 -0300

    Update index.js

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-        //Posso colocar qualquer nome, sendo com letra maiuscula
+//Posso colocar qualquer nome, sendo com letra maiuscula
 // import Primeiro from './componentes/Primeiro'
 
-// import BomDia from './componentes/BomDia'
 
-import { BoaTarde, BoaNoite } from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
+
+ReactDOM.render(
+        <div>
+                <Saudacao tipo="Bom dia" nome="João"/>
+        </div>
+, document.getElementById('root'))
+
+
+
+// import { BoaTarde, BoaNoite } from './componentes/Multiplos'
+
+// ReactDOM.render(
+//         <div>
+//                 <BoaTarde nome="Renan" />
+//                 <BoaNoite nome="Carlos" />
+//         </div>
+// , document.getElementById('root'))
+
+
+
+// import BomDia from './componentes/BomDia'
 
 // const elemento = <h1>React 3</h1>
 // ReactDOM.render(<BomDia nome="Renan" />, document.getElementById('root'))
 
-ReactDOM.render(
-        <div>
-                <BoaTarde nome="Renan" />
-                <BoaNoite nome="Carlos" />
-        </div>
-, document.getElementById('root'))
