f853b3ff6ed6d07b0b4ba7fbed3a441b58d74006
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-02-25 18:56:54 -0300

    Update Saudacao.jsx

--- exercicios/src/componentes/Saudacao.jsx
+++ exercicios/src/componentes/Saudacao.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from  'react'
 
+//Exportar por padrão a classe Saudacao
 export default class Saudacao extends Component {
 
+    state = {
+        tipo: this.props.tipo,
+        nome: this.props.nome
+    }
+    
     setTipo(e) {
-        console.log(e.target.value)
+        this.setState({ tipo: e.target.value }) 
     }
 
+    setNome(e){
+        this.setState({ nome: e.target.value })
+    }
+
+    //Resposável por renderizar o componente
     render() {
-        const { tipo, nome } = this.props
-        return (
+        const { tipo, nome } = this.state //Uso this.props para acessar a propriedades do objeto atual
+        return ( //Entre parentes jás a expressão
             <div>
                 <h1>{tipo} {nome}</h1>
                 <hr/>
                 <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)}/>
-                <input type="text" placeholder="nome"  value={nome}/>
+                <input type="text" placeholder="nome"  value={nome} onChange={e => this.setNome(e)}/>
             </div>
         )
     }
