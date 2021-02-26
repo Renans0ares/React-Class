b95890faf9bdc080a07ae2dd1c117fafe09a2200
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-02-26 18:27:56 -0300

    Add files via upload

---
+++ exercicios/src/componentes/Pai.jsx
@@ -0,0 +1,13 @@
+import React from  'react'
+// import Filho from './Filho'
+import { childrenWithProps } from '../Utils/Util'
+
+
+export default props => 
+    <div>
+        <h1>{props.nome} {props.sobrenome}</h1>
+        <h2>Filhos</h2>
+        <ul>
+            {childrenWithProps(props)} {/* Vai Passar os componentes do pais para os filhos */}
+        </ul>
+    </div>
