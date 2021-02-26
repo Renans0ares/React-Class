4a6a35b77cc929b78530ab742ea8d4ce4f68b372
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-02-26 17:24:30 -0300

    Update Saudacao.jsx

--- exercicios/src/componentes/Saudacao.jsx
+++ exercicios/src/componentes/Saudacao.jsx
@@ -7,6 +7,12 @@ export default class Saudacao extends Component {
         tipo: this.props.tipo,
         nome: this.props.nome
     }
+
+    constructor(props){
+        super(props)    
+
+        this.setTipo = this.setTipo.bind(this)
+    }
     
     setTipo(e) {
         this.setState({ tipo: e.target.value }) 
@@ -23,9 +29,9 @@ export default class Saudacao extends Component {
             <div>
                 <h1>{tipo} {nome}</h1>
                 <hr/>
-                <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)}/>
-                <input type="text" placeholder="nome"  value={nome} onChange={e => this.setNome(e)}/>
+                <input type="text" placeholder="Tipo..." value={tipo} onChange={this.setTipo} />
+                <input type="text" placeholder="nome"  value={nome} onChange={e => this.setNome(e)} />
             </div>
-        )
+        )   
     }
 }
