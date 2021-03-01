3a49053fb3e69b98c105b1015a880fa3ba7c2ca8
Renan Soares Sousa <60124149+Renans0ares@users.noreply.github.com>
2021-03-01 18:06:43 -0300

    Update Button.jsx

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>{
-    let classes = 'button '
-    classes += props.operation ? 'operation' : ''
+export default props =>{ //Componente funcional sem estado
+    let classes = 'button ' /*Gerando as classes a partir da variavel */
+    classes += props.operation ? 'operation' : ''/*Se a propriedade for passada será adicionado, se não será vazio*/
     classes += props.double ? 'double': ''
     classes += props.triple ? 'triple' : ''
     return (
         <button
-        onClick={e => props.click && props.click(e.target.innerHTML)} 
+        onClick={e => props.click && props.click(e.target.innerHTML)} //Evento do CLick passando o conteudo do click
         className={classes}>
         {props.label}
     </button>
     )
 }
-    
